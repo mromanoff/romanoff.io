599f5a2da90ba1d32cdea247a7f36f3727c69d21
Michael Romanoff <zelcie@gmail.com>
2015-01-25 18:55:35 -0500

    adding new images for hero container

--- src/overview/hero/collection.js
+++ src/overview/hero/collection.js
@@ -15,10 +15,10 @@ module.exports = new Collection([
     alt: 'romanoff.io'
   },
   {
-    small: '/img/hero/main-2-768x720.jpg',
-    medium: '/img/hero/main-2-1280x600.jpg',
-    large: '/img/hero/main-2-1920x700.jpg',
-    extraLarge: '/img/hero/main-2-1920x700.jpg',
+    small: '/img/hero/main-3-768x720.jpg',
+    medium: '/img/hero/main-3-1280x600.jpg',
+    large: '/img/hero/main-3-1920x700.jpg',
+    extraLarge: '/img/hero/main-3-1920x700.jpg',
     title: 'Test before you deploy',
     alt: 'romanoff.io'
   },
@@ -27,14 +27,14 @@ module.exports = new Collection([
     medium: '/img/hero/main-2-1280x600.jpg',
     large: '/img/hero/main-2-1920x700.jpg',
     extraLarge: '/img/hero/main-2-1920x700.jpg',
-    title: 'Stop talking start working',
+    title: 'Stop talking start walking',
     alt: 'romanoff.io'
   },
   {
-    small: '/img/hero/main-2-768x720.jpg',
-    medium: '/img/hero/main-2-1280x600.jpg',
-    large: '/img/hero/main-2-1920x700.jpg',
-    extraLarge: '/img/hero/main-2-1920x700.jpg',
+    small: '/img/hero/main-4-768x720.jpg',
+    medium: '/img/hero/main-4-1280x600.jpg',
+    large: '/img/hero/main-4-1920x700.jpg',
+    extraLarge: '/img/hero/main-4-1920x700.jpg',
     title: 'Take a picture before too late',
     alt: 'romanoff.io'
   }
